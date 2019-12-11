e5cc717c7696ec26d5b4493321fd0650f570d903
dev1 <dev1@wwt.com>
2019-12-10 17:17:25 -0700

    moved code that assembles response into the traffic class and added a test

--- api/traffic/traffic_handler.py
+++ api/traffic/traffic_handler.py
@@ -1,13 +1,21 @@
 import json
 from traffic.keystone_traffic import KeystoneTraffic
+import urllib
 
 
 def trafficHandler(event, context):
     content = {}
-    keystone = KeystoneTraffic("2300 15th st, Denver, CO, 80202")
-    keystone.make_google_api_call()
     try:
-        content["TrafficReport"] = keystone.duration
+      user_location = event["queryStringParameters"]["location"]
+    except:
+      user_location = urllib.parse.quote("2300 15th st, Denver, CO, 80202")
+
+    user_location = urllib.parse.unquote(user_location)
+    keystone = KeystoneTraffic(user_location)
+    keystone.make_google_api_call()
+    
+    try:
+        content["TrafficReport"] = keystone.get_response()
         status = 200
     except Exception:
         content["TrafficReport"] = "Unavailable"
