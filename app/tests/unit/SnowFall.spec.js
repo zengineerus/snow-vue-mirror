9ec9d6da5df560f702bd1cc58a93d48a61626d56
Bench <brent.marquez@asynchrony.com>
2019-12-19 17:02:14 -0700

    Snow fall test

--- app/tests/unit/SnowFall.spec.js
+++ app/tests/unit/SnowFall.spec.js
@@ -2,8 +2,35 @@ import { mount } from '@vue/test-utils';
 import SnowFall from '@/components/SnowFall';
 import Vue from 'vue'
 
+jest.mock('@/services/snow-data', () => ({ 
+    travelTime: jest.fn(),
+    snowReports: () => Promise.resolve({
+        body: {
+            SnowReport: [
+                {
+                    Depth: 
+                    {
+                        Inches: 11
+                    }
+                },
+                {
+                    Depth:
+                    {
+                        Inches: 22
+                    }
+                }
+            ]
+        }
+    })
+}));
+
 describe('SnowFall.vue', () => {
-    // test('true', () => {
-    //     expect(true).toBe(true);
-    // })
+    test('should show snow data', async() => {
+
+        const wrapper = mount(SnowFall);
+        expect(wrapper.isVueInstance()).toBeTruthy();
+        await Vue.nextTick();
+
+        expect(wrapper.text()).toContain('Overnight SnowFall 24 Hour Snow 11 inches 22 inches');
+    })
 });
